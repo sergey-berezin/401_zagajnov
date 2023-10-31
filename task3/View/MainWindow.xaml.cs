b744019930328252e17bbead1914a841129ee200
Zagaynov Sergey <zagainov_sd@gkl-kemerovo.ru>
2023-10-31 08:42:40 +0300

    task3

---
+++ task3/View/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewModel;
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IUIServices
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            DataContext = new MainViewModel(this);
+        }
+        //public void ReportError(string message)
+        //{
+        //    errorBar.Text = message;
+        //    stack.Visibility = Visibility.Visible;
+        //}
+        public string OpenSaveDialog()
+        {
+            return null;
+        }
+        public string[] OpenLoadDialog()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == true)
+            {
+                return dlg.FileNames;
+            }
+            return null;
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    stack.Visibility = Visibility.Collapsed;
+        //}
+    }
+}
---
+++ task3/ViewModel/AsyncRelayCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object, Task> execute;
+        private readonly Func<object, bool> canExecute;
+        private bool is_busy { get; set; }
+        public AsyncRelayCommand(Func<object, Task> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (is_busy)
+                return false;
+            else
+                return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!is_busy)
+            {
+                is_busy = true;
+                execute?.Invoke(parameter).ContinueWith(_ =>
+                {
+                    is_busy = false;
+                    CommandManager.InvalidateRequerySuggested();
+                }, CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+
+    }
+}
