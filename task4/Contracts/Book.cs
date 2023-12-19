c5f0126db1c4beb981a66f7aa5303410545d44a6
Zagaynov Sergey <zagainov_sd@gkl-kemerovo.ru>
2023-12-19 08:00:33 +0300

    task 4

---
+++ task4/Contracts/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contracts
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public int Pages { get; set; }
+    }
+}
---
+++ task4/Web/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using YamlDotNet.Core.Tokens;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerDocument();
+            
+            services.AddSingleton<IImageProcessor>(new Processor());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
